70d3a136f4afdd18f3599020f7f5be735cdcac57
rkartzman & Scott Enriquez <pair+rkartzman.sjenriquez@devbootcamp.com>
2015-01-09 17:34:22 -0500

    read JSON successful

--- ar-skeleton/db/seeds.rb
+++ ar-skeleton/db/seeds.rb
@@ -1,19 +1,28 @@
+require 'json'
+require 'pp'
 
-toilet1_details = { :name => "Central Park",
-            :location => "93d Street near West Drive",
-            :open_year_round => 1,
-            :handicap_accessible => 0}
+# toilet1_details = { :name => "Central Park",
+#             :location => "93d Street near West Drive",
+#             :open_year_round => 1,
+#             :handicap_accessible => 0}
 
-toilet2_details = { :name => "Battery Park",
-            :location => "Battery Place, State and Whitehall streets",
-            :open_year_round => 0,
-            :handicap_accessible => 1}
+# toilet2_details = { :name => "Battery Park",
+#             :location => "Battery Place, State and Whitehall streets",
+#             :open_year_round => 0,
+#             :handicap_accessible => 1}
 
 
-Toilet.create(toilet1_details)
-Toilet.create(toilet2_details)
+# Toilet.create(toilet1_details)
+# Toilet.create(toilet2_details)
 
-Borough.create([
-  { borough_name: "Manhattan", toilet_id: 1 },
-  { borough_name: "Bronx", toilet_id: 2 }
-])
+# Borough.create([
+#   { borough_name: "Manhattan", toilet_id: 1 },
+#   { borough_name: "Bronx", toilet_id: 2 }
+# ])
+
+file = APP_ROOT.join('db', 'data.json')
+
+obj = File.read(file)
+parsed_objects = JSON.parse(obj)
+
+pp parsed_objects
